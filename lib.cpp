bb9bb99eccaaf2f2fabb1c942cfa1ec7191e3065
RGioele <116067942+RGioele@users.noreply.github.com>
2022-12-09 10:33:52 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,14 @@
 
+#include "lib.h"
+bool converti_lettera (char & a){
+    if (a>=65 && a<=90){
+        a +=32;
+        return true;
+    }
+    else if(a>=97 && a<=122){
+        a -= 32;
+        return true;
+    }
+    else
+    return false;
+}
